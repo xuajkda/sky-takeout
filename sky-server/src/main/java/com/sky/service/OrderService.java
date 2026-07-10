ec4d7cd3ba7cc9e8c63d333e7c0bd0f418585be6
cuiweixuan <linghua0214un@gmail.com>
2026-07-10 19:56:41 +0800

    取消订单业务代码开发

--- sky-server/src/main/java/com/sky/service/OrderService.java
+++ sky-server/src/main/java/com/sky/service/OrderService.java
@@ -39,4 +39,10 @@ public interface OrderService {
      * @return
      */
     OrderVO details(Long id);
+
+    /**
+     * 用户根据 id取消订单
+     * @param id
+     */
+    void userCancelById(Long id) throws Exception;
 }
